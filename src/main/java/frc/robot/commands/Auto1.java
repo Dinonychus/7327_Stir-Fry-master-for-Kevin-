808cf9116b84983aafb68c9fa655885095f78b81
Dinonychus <72425800+Dinonychus@users.noreply.github.com>
2023-02-25 01:28:39 -0800

    Replaced example stuff

--- src/main/java/frc/robot/commands/Auto1.java
+++ src/main/java/frc/robot/commands/Auto1.java
@@ -4,23 +4,19 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+public class Auto1 extends CommandBase {
+  /** Creates a new Auto1. */
+  private final DriveTrain driveTrain;
+
+  public Auto1(DriveTrain driveTrain) {
+
+    this.driveTrain = driveTrain;
+    addRequirements(driveTrain);
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
